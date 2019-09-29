beb9320f303c36ab41173d7a957823050f143e0b
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-09-29 19:34:46 +0000

    first ...

---
+++ Swiper/Swiper/Controls/SwiperControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Swiper.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Swiper.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwiperControl : ContentView
+    {
+        private readonly double _initialRotation;
+        private static readonly Random _random = new Random();
+        public SwiperControl()
+        {
+            InitializeComponent();
+
+            var picture = new Picture();
+            var panGesture = new PanGestureRecognizer();
+
+            descriptionLabel.Text = picture.Description;
+
+            image.Source = new UriImageSource() { Uri = picture.Uri };
+
+            loadingLabel.SetBinding(IsVisibleProperty, "IsLoading");
+            loadingLabel.BindingContext = image;
+
+            panGesture.PanUpdated += OnPanUpdated;
+            GestureRecognizers.Add(panGesture);
+
+            _initialRotation = _random.Next(-10, 10);
+            photo.RotateTo(_initialRotation, 100, Easing.SinOut);
+        }
+
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    PanStarted();
+                    break;
+                case GestureStatus.Running:
+                    PanRunning(e);
+                    break;
+                case GestureStatus.Completed:
+                    PanCompleted();
+                    break;
+            }
+        }
+        private void PanStarted()
+        {
+            photo.ScaleTo(1.1, 100);
+        }
+        private void PanRunning(PanUpdatedEventArgs e)
+        {
+            photo.TranslationX = e.TotalX;
+            photo.TranslationY = e.TotalY;
+            photo.Rotation = _initialRotation + (photo.TranslationX / 25);
+        }
+        private void PanCompleted()
+        {
+            photo.TranslateTo(0, 0, 250, Easing.SpringOut);
+            photo.RotateTo(_initialRotation, 250, Easing.SpringOut);
+            photo.ScaleTo(1, 250);
+        }
+
+        
+
+        
+    }
+}
---
+++ Swiper/Swiper/Utils/DescriptionGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Swiper.Utils
+{
+    public class DescriptionGenerator
+    {
+        private string[] _adjectives = { "nice", "horrible", "great", "terrible old", "brand new" };
+        private string[] _other = { "picture of something", "car", "photo of a forest", "duck" };
+        private static Random random = new Random();
+
+        public string Generate()
+        {
+            var a = _adjectives[random.Next(_adjectives.Count())];
+            
+            var b = _other[random.Next(_other.Count())];
+            
+            return $"A {a} {b}";
+        }
+    }
+}
