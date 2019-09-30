315473e93d71c163a17735d5cf172df4135990c9
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-09-30 15:15:11 +0000

    Done

--- Swiper/Swiper/Controls/SwiperControl.xaml.cs
+++ Swiper/Swiper/Controls/SwiperControl.xaml.cs
@@ -13,8 +13,17 @@ namespace Swiper.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SwiperControl : ContentView
     {
+        public event EventHandler OnLike;
+        public event EventHandler OnDeny;
+
+        private double _screenWidth = -1;
+        private const double DeadZone = 0.4d;
+        private const double DecisionThreshold = 0.4d;
         private readonly double _initialRotation;
         private static readonly Random _random = new Random();
+        private static double Clamp(double value, double min, double max)
+        { return (value < min) ? min : (value > max) ? max : value; }
+
         public SwiperControl()
         {
             InitializeComponent();
@@ -34,6 +43,60 @@ namespace Swiper.Controls
 
             _initialRotation = _random.Next(-10, 10);
             photo.RotateTo(_initialRotation, 100, Easing.SinOut);
+        }
+
+        private void CalculatePanState(double panX)
+        {
+            var halfScreenWidth = _screenWidth / 2;
+            var deadZoneEnd = DeadZone * halfScreenWidth;
+
+            if (Math.Abs(panX) < deadZoneEnd)
+                return;
+
+            var passedDeadzone = panX < 0 ? panX + deadZoneEnd : panX - deadZoneEnd;
+            var decisionZoneEnd = DecisionThreshold * halfScreenWidth;
+            var opacity = passedDeadzone / decisionZoneEnd;
+
+            opacity = Clamp(opacity, -1, 1);
+
+            likeStackLayout.Opacity = opacity;
+            denyStackLayout.Opacity = -opacity;
+        }
+
+        private bool CheckForExitCriteria()
+        {
+            var halfScreenWidth = _screenWidth / 2;
+            var decisionBreakpoint = DeadZone * halfScreenWidth;
+            return (Math.Abs(photo.TranslationX) > decisionBreakpoint);
+        }
+
+        private void Exit()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var direction = photo.TranslationX < 0 ? -1 : 1;
+
+                if (direction > 0)
+                    OnLike?.Invoke(this, new EventArgs());
+
+                if (direction < 0)
+                    OnDeny?.Invoke(this, new EventArgs());
+
+                await photo.TranslateTo(photo.TranslationX + (_screenWidth * direction), photo.TranslationY, 200, Easing.CubicIn);
+
+                var parent = Parent as Layout<View>;
+                parent?.Children.Remove(this);
+            });
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (MainPage == null)
+                return;
+
+            _screenWidth = MainPage.Width;
         }
 
         private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
@@ -60,12 +123,25 @@ namespace Swiper.Controls
             photo.TranslationX = e.TotalX;
             photo.TranslationY = e.TotalY;
             photo.Rotation = _initialRotation + (photo.TranslationX / 25);
+
+            CalculatePanState(e.TotalX);
         }
         private void PanCompleted()
         {
+            if (CheckForExitCriteria())
+                Exit();
+
+            likeStackLayout.Opacity = 0;
+            denyStackLayout.Opacity = 0;
+
             photo.TranslateTo(0, 0, 250, Easing.SpringOut);
             photo.RotateTo(_initialRotation, 250, Easing.SpringOut);
             photo.ScaleTo(1, 250);
+        }
+
+        private Page MainPage
+        {
+            get { return Application.Current.MainPage; }
         }
 
         
--- Swiper/Swiper/MainPage.xaml.cs
+++ Swiper/Swiper/MainPage.xaml.cs
@@ -14,10 +14,52 @@ namespace Swiper
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private int _likeCount;
+        private int _denyCount;
+
         public MainPage()
         {
             InitializeComponent();
+
+            AddInitialPhotos();
+
             MainGrid.Children.Add(new SwiperControl());
+        }
+
+        private void UpdateGui()
+        {
+            likeLabel.Text = _likeCount.ToString();
+            denyLabel.Text = _denyCount.ToString();
+        }
+
+        private void Handle_OnLike(object sender, EventArgs e)
+        {
+            _likeCount++;
+            InsertPhoto();
+            UpdateGui();
+        }
+
+        private void Handle_OnDeny(object sender, EventArgs e)
+        {
+            _denyCount++;
+            InsertPhoto();
+            UpdateGui();
+        }
+
+        private void AddInitialPhotos()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                InsertPhoto();
+            }
+        }
+
+        private void InsertPhoto()
+        {
+            var photo = new SwiperControl();
+            photo.OnDeny += Handle_OnDeny;
+            photo.OnLike += Handle_OnLike;
+            this.MainGrid.Children.Insert(0, photo);
         }
     }
 }
